e7ba0e38c9cf45206ac10b04fe1b54fdb1a0d458
Giovanni <g.salmeri@mondodomani.org>
2021-03-14 17:30:44 +0100

    Validate JSON

--- chartjs.js
+++ chartjs.js
@@ -2,11 +2,20 @@
 window.addEventListener("load", function() {
     var charts = document.querySelectorAll("pre.chartjs");
     for (var i = 0; i < charts.length; i++) {
+	try {
+		JSON.parse(charts[i].firstChild.textContent.replace(/\/\/.*/gm, ""));
+	} catch (e) {
+		charts[i].firstChild.textContent = "Invalid JSON";
+		charts[i].style.display = "block";
+		return;
+	}
         var canvas = document.createElement("canvas");
         canvas.id = "chartjs"+i;
 	var dims = charts[i].firstChild.textContent.match(/^\/\/\s*(\d+)\s+(\d+)/);
-        canvas.width = dims[1];
-        canvas.height = dims[2];
+	if (dims) {
+		canvas.width = dims[1];
+		canvas.height = dims[2];
+	}
         var script = document.createElement("script");
         script.text = "var ctx = document.getElementById('"+canvas.id+"').getContext('2d');\n"
         script.text += "var myChart = new Chart(ctx, "+charts[i].firstChild.textContent+");";
